aa8656d398881c539b93617be6596b733297304e
4Forsythe <forsythe.dev@gmail.com>
2025-02-22 21:11:30 +0400

    Upd: add css-modules into vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      scopeBehaviour: 'local',
+      localsConvention: 'camelCaseOnly',
+      generateScopedName: '[name]_[local]__[hash:base64:5]',
+    },
+  },
   resolve: {
     alias: {
       '@': '/src',
